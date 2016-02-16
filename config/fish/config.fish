68c5d39fc56a2f5dfb58cc045a9b05dff8fccd91
Grant Rodgers <grantr@gmail.com>
2016-02-16 14:38:04 -0800

    add fish config

---
+++ config/fish/config.fish
@@ -0,0 +1,14 @@
+# autojump
+
+[ -f /usr/local/share/autojump/autojump.fish ]; and . /usr/local/share/autojump/autojump.fish
+
+# git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch yellow
+
+# go
+set -x GOPATH $HOME/go
+set -x GO15VENDOREXPERIMENT 1
+set -x PATH $GOPATH/bin $PATH
