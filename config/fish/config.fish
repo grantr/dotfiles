bfb1a069bcf67333f45d84d4f7a7b92ff138f256
Grant Rodgers <grantr@gmail.com>
2016-05-17 13:16:53 -0700

    updates
    add grpc
    add rbenv config to fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -12,3 +12,7 @@ set __fish_git_prompt_color_branch yellow
 set -x GOPATH $HOME/go
 set -x GO15VENDOREXPERIMENT 1
 set -x PATH $GOPATH/bin $PATH
+
+# rbenv
+set PATH $HOME/.rbenv/shims $PATH
+rbenv rehash >/dev/null ^&1
